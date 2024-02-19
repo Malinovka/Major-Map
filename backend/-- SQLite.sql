31f12f1c3110dc2d81c41ee4d8ff9944f20a7f24
jackywangg <153401976+jackywangg@users.noreply.github.com>
2024-02-18 23:05:56 -0800

    Tables Feb 17

---
+++ backend/-- SQLite.sql
@@ -0,0 +1,21 @@
+-- SQLite
+--INSERT INTO Major (name, year)
+--VALUES ('Biology', 2024)
+
+-- SELECT * FROM Major;
+
+--CREATE TABLE Course (
+--            Course_Name TEXT NOT NULL,
+--            Course_Code_Name TEXT NOT NULL,
+--            Course_Code_Number INTEGER NOT NULL,
+--            Credits INTEGER NOT NULL,
+--            Description TEXT NOT NULL
+--);
+
+--INSERT INTO Course (Course_Name, Course_Code_Name, Course_Code_Number, Credits, Description)
+--VALUES ('Models of Computation', 'CPSC', 121, 4, 'Physical and mathematical structures of computation. Boolean algebra and combinations logic circuits; proof techniques; functions and sequential circuits; sets and relations; finite state machines; sequential instruction execution.')
+
+SELECT * FROM Course
+
+
+
--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,37 @@ app = FastAPI()
 
 
 @app.get("/")
-async def get_sqlite_version():
+async def get_sqlite_data():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT sqlite_version()")
-    result = cursor.fetchone()[0]
+    
+    cursor.execute("SELECT * FROM Major")
+    major_rows = cursor.fetchall()
+    
+    cursor.execute("SELECT * FROM Course")
+    course_rows = cursor.fetchall()
+    
     conn.close()
-    return {"message": result}
+
+    major_data = []
+    for row in major_rows:
+        row_data = {"name": row[0], "year": row[1]}
+        major_data.append(row_data)
+
+    course_data = []
+    for row in course_rows:
+        row_data = {"Course_Name": row[0], "Course_Code_Name": row[1], "Course_Code_Number": row[2], "Credits": row[3], "Description": row[4]}
+        course_data.append(row_data)
+
+    return {"major_data": major_data, "course_data": course_data}
+
+
+#async def get_sqlite_version():
+#    conn = sqlite3.connect("database.db")
+#    cursor = conn.cursor()
+ #   cursor.execute("SELECT sqlite_version()")
+ #   result = cursor.fetchone()[0]
+ #   conn.close()
+ #   return {"message": result}
+
+
