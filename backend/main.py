05ead6eef58d104a766d31fb63f1b83755aed62a
jackywangg <153401976+jackywangg@users.noreply.github.com>
2024-03-04 20:50:19 -0800

    GET
    Created GET paths for Major and Courses

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ class Courses(BaseModel):
     credits: int
     description: str
 
-@app.get("/")
+@app.get("/major/")
 async def get_sqlite_data():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
@@ -24,10 +24,6 @@ async def get_sqlite_data():
     major_rows = cursor.fetchall()
     print(len(major_rows))
 
-    cursor.execute("SELECT * FROM Courses")
-    course_rows = cursor.fetchall()
-    print(len(course_rows))
-    
     conn.close()
 
     major_data = []
@@ -35,12 +31,25 @@ async def get_sqlite_data():
         row_data = {"name": row[0], "year": row[1]}
         major_data.append(row_data)
 
+    return {"major_data": major_data}
+
+@app.get("/courses/") 
+async def get_sqlite_data():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Courses")
+    course_rows = cursor.fetchall()
+    print(len(course_rows))
+    
+    conn.close()
+    
     course_data = []
     for row in course_rows:
         row_data = {"Courses_Name": row[0], "Courses_Code_Name": row[1], "Courses_Code_Number": row[2], "Number_of_Credits": row[3], "Course_Description": row[4]}
         course_data.append(row_data)
 
-    return {"major_data": major_data, "course_data": course_data}
+    return {"course_data": course_data}
 
 @app.post("/major/", response_model=Major, status_code=201)
 async def create_major(major: Major):
